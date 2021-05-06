4dcaebc03608b0fb5aa7e39f41765cc42ec4f4a5
madsur <dennissheets@outlook.com>
2021-05-06 15:25:45 -0500

    work in progress

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import org.junit.Test;
-
+import main.BonusBinarySearch;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
---
+++ src/test/BonusBinarySearchTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import main.BonusBinarySearch;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+import static org.junit.Assert.*;
+
+public class BonusBinarySearchTest {
+
+    @Test
+    public void emptyTest() {
+        assertEquals(true, true);
+    }
+
+//    @Test
+//    public void hasIntReturnsTrue() {
+//        int[] array = {1,2,3,4,5,6,7,8,9};
+//        assertTrue(BonusBinarySearch.binarySearch(array, 5));
+//    }
+
+//    @Test
+//    public void missingIntReturnsFalse() {
+//        int[] array = {1,2,3,4,5,6,7,8,9};
+//        assertEquals(BonusBinarySearch.binarySearch(array, 11));
+//    }
+
+}
