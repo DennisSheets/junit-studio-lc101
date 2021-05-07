9c80fba03decc6773e3de2054e1b3eff34b3a130
madsur <dennissheets@outlook.com>
2021-05-07 15:43:09 -0500

    bonus 2 completed

--- src/main/BonusBinarySearch.java
+++ src/main/BonusBinarySearch.java
@@ -10,20 +10,64 @@ public class BonusBinarySearch {
      * @param sortedNumbers - must be sorted from least to greatest
      * @param n - number to search for
      * @return index of search item if it's found, -1 if not found
+     *
      */
-    public static int binarySearch(int[] sortedNumbers, int n) {
-        int right = sortedNumbers.length - 1;
+//  search for  n=5         mid-n
+//      r=9 l=0     mid=4.5 (4)     i4=5 5>5     r=5
+//      r=5 l=0     mid=2.5 (2)     i3=4 4<5     l=3
+//      r=5 l=3     mid=4           i4=5 5=5     return 4  as in i4 which is a value of 5
+
+//  search for  n=7
+//      r=9 l=0    mid=4.5 (4)     mid4=6 6<7  l=4
+//      r=9 l=5    mid=(7)     mid7=8 8>7  r=7
+//      r=7 l=5    mid=(6)     mid6=8 8>7  r=6
+//      r=6 l=5    mid=
+
+//  search for n=10
+//      r=9 l=0    mid=4.5 (4)  mid4=5  6<7 l=4
+//      r=9 l=4    mid=(6)  mid6=8   8<10  l=6
+//      r=9 l=6    mid=(7)   mid7=8  8<10  l=7
+//      r=9 l=7    mid=(8)   mid8=9  9<10  l=8
+//      r=9 l=8    mid=(8)   mid8=9  9<10  l=8
+
+    public static int binarySearch(int[] sortedNumbers, int n) {      // int[] array = {1, 2, 3, 4, 5, 6, 8, 9, 10, 15}    length of 10
+        int right = sortedNumbers.length - 1;                         // 9              0  1  2  3  4  5  6  7  8  9
         int left = 0;
-        while (right >= left) {
-            int mid = left + ((right - left) / 2);
-            if (sortedNumbers[mid] > n) {
-                right = mid;
-            } else if (sortedNumbers[mid] < n) {
-                left = mid;
-            } else {
-                return mid;
+        int x = 0;
+
+
+        while (right - left > 1) {
+            System.out.println("      " + right +" - "+ left +" > " + x + ": " + (right - left > x));
+
+                int mid = left + ((right - left) / 2);              // 0 + (( 9 + 0) / 2 )    mid = 4.5     5
+                if ( n == sortedNumbers[sortedNumbers.length -1]){
+                    System.out.println("            " + n + " = " + sortedNumbers[(sortedNumbers.length - 1)] + " returned last index:" + (sortedNumbers.length - 1)  + "\n");
+                    return sortedNumbers.length -1;
+                }else if ( n == sortedNumbers[0]){
+                System.out.println("            " + n + " = " + sortedNumbers[0] + " returned first index:" + (0)  + "\n");
+                return 0;
             }
-        }
+
+                else if (sortedNumbers[mid] > n) {
+                    right = mid;
+                    System.out.println("l=" + left +  " r=" + right + " mid=" + mid + " array[mid]" + sortedNumbers[mid] + " > n=" + n);
+
+                } else if (sortedNumbers[mid] < n) {
+                    left = mid;
+                    System.out.println("l=" + left + " r=" + right + " mid=" + mid + " array[mid]" + sortedNumbers[mid] + " < n=" + n);
+
+
+                } else {
+                    System.out.println("l=" + left +  " r=" + right + " mid=" + mid + " array[mid]" + sortedNumbers[mid] + " = n=" + n);
+                    System.out.println("            " + n + " = " + sortedNumbers[mid] + " returned mid:" + mid  + "\n");
+                    return mid;
+
+               }
+
+
+            }
+        System.out.println("return: -1 (While failed)");
+        System.out.println("            " + n + " can't be found between " + sortedNumbers[left] + " and " + sortedNumbers[right] + "\n");
         return -1;
     }
 
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+//import org.junit.Before;
+import org.junit.Before;
 import org.junit.Test;
 import main.BonusBinarySearch;
 import static org.junit.Assert.*;
@@ -7,10 +9,56 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+    public int[] array = {1, 2, 3, 4, 5, 6, 8, 9, 10, 15};
+    //                    0  1  2  3  4  5  6  7  8  9
+
+//    @Before
+//    public void createArray() {
+//        int[] array = {1, 2, 3, 4, 5, 6, 8, 8, 9, 10};
+//    }
+
+//    @Test
+//    public void TestBemptyTest() {
+//        assertEquals(true, true);
+//    }
+//
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void TestHas10True() {
+        assertEquals(8,BonusBinarySearch.binarySearch(array, 10),.01);
     }
 
+    @Test
+    public void TestHas5True() {
+        assertEquals(4,BonusBinarySearch.binarySearch(array, 5),.01);
+    }
+
+    @Test
+    public void TestHas1True() {
+        assertEquals(0,BonusBinarySearch.binarySearch(array, 1),.01);
+    }
+
+    @Test
+    public void TestMissing7True() {   //  return -1
+        assertEquals(-1,BonusBinarySearch.binarySearch(array, 7),.01);
+
+    }
+
+    @Test
+    public void TestMissing11True() {   //  return -1
+        assertEquals(-1,BonusBinarySearch.binarySearch(array, 11),.01);
+
+    }
+
+    @Test
+    public void TestMissing0True() {   //  return -1
+        assertEquals(-1,BonusBinarySearch.binarySearch(array, 0),.01);
+
+    }
+
+    @Test
+    public void TestMissing100True() {   //  return -1
+        assertEquals(-1,BonusBinarySearch.binarySearch(array, 100),.01);
+
+    }
 
 }
